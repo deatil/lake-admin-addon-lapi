1b1f777594e61e21121e732931c7e9e3867b8198
deatil <2217957370@qq.com>
2021-02-02 13:10:14 +0800

    更新

--- src/addon/lapi/info.php
+++ src/addon/lapi/info.php
@@ -9,7 +9,6 @@ return [
     'authoremail' => 'deatil@github.com',
     'version' => '2.0.2',
     'adaptation' => '2.0.2',
-    'sign' => 'b15cc279e3484c13c96c2f7142e2f437',
     
     'path' => __DIR__,
     
@@ -22,15 +21,6 @@ return [
     
     // 菜单
     'menus' => include __DIR__ . '/menu.php',
-    
-    // 数据表，不用加表前缀
-    'tables' => [
-        'lapi_config',
-        'lapi_app',
-        'lapi_app_log',
-        'lapi_url',
-        'lapi_url_access',
-    ],
     
     // 安装演示数据
     'demo' => 0,
